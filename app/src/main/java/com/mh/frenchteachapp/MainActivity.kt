9b14b43544d6bbbfe6e7e505f2bedc6ecd69f5c6
Marcelo Hirota <marcelohirota@gmail.com>
2023-05-03 12:15:13 +0930

    Added functionalities

--- app/src/main/java/com/mh/frenchteachapp/MainActivity.kt
+++ app/src/main/java/com/mh/frenchteachapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.mh.frenchteachapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    public fun SayColour(view: View){
+        val clickedButton : Button = view as Button
+
+        var mediaPlayer = MediaPlayer.create(
+            this,
+            resources.getIdentifier(
+                clickedButton.tag.toString(),
+                "raw",
+                packageName))
+        mediaPlayer.start()
     }
 }
